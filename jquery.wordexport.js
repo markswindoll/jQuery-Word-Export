ee91590a320bcbf82a3ab9b3d5078a2cc6e384f1
Mark Swindoll <mark@swindoll.us>
2014-05-05 17:21:49 -0500

    Initial commit of source code

---
+++ jquery.wordexport.js
@@ -0,0 +1,86 @@
+if (jQuery && saveAs) {
+    (function($) {
+        $.fn.wordExport = function(fileName) {
+            var static = {
+                mhtml: {
+                    top: "Mime-Version: 1.0\nContent-Base: " + location.href + "\nContent-Type: Multipart/related; boundary=\"NEXT.ITEM-BOUNDARY\";type=\"text/html\"\n\n--NEXT.ITEM-BOUNDARY\nContent-Type: text/html; charset=\"utf-8\"\nContent-Location: " + location.href + "\n\n<!DOCTYPE html>\n<html>\n_html_</html>",
+                    head: "<head>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n<style>\n_styles_\n</style>\n</head>\n",
+                    body: "<body>_body_</body>"
+                }
+            };
+            var options = {
+                maxWidth: 624
+            };
+            // Clone selected element before manipulating it
+            var markup = $(this).clone();
+
+            // Remove hidden elements from the output
+            markup.each(function() {
+                var self = $(this);
+                if (self.is(':hidden'))
+                    self.remove();
+            });
+
+            // Embed all images using Data URLs
+            var images = Array();
+            var img = markup.find('img');
+            for (var i = 0; i < img.length; i++) {
+                // Calculate dimensions of output image
+                var w = Math.min(img[i].width, options.maxWidth);
+                var h = img[i].height * (w / img[i].width);
+                // Create canvas for converting image to data URL
+                $('<canvas>').attr("id", "jQuery-Word-export_img_" + i).width(w).height(h).insertAfter(img[i]);
+                var canvas = document.getElementById("jQuery-Word-export_img_" + i);
+                canvas.width = w;
+                canvas.height = h;
+                // Draw image to canvas
+                var context = canvas.getContext('2d');
+                context.drawImage(img[i], 0, 0, w, h);
+                // Get data URL encoding of image
+                var uri = canvas.toDataURL();
+                $(img[i]).attr("src", img[i].src);
+                img[i].width = w;
+                img[i].height = h;
+                // Save encoded image to array
+                images[i] = {
+                    type: uri.substring(uri.indexOf(":") + 1, uri.indexOf(";")),
+                    encoding: uri.substring(uri.indexOf(";") + 1, uri.indexOf(",")),
+                    location: $(img[i]).attr("src"),
+                    data: uri.substring(uri.indexOf(",") + 1)
+                };
+                // Remove canvas now that we no longer need it
+                canvas.parentNode.removeChild(canvas);
+            }
+
+            // Prepare bottom of mhtml file with image data
+            var mhtmlBottom = "";
+            for (var i = 0; i < images.length; i++) {
+                mhtmlBottom += "--NEXT.ITEM-BOUNDARY\n";
+                mhtmlBottom += "Content-Location: " + images[i].contentLocation + "\n";
+                mhtmlBottom += "Content-Type: " + images[i].contentType + "\n";
+                mhtmlBottom += "Content-Transfer-Encoding: " + images[i].contentEncoding + "\n\n";
+                mhtmlBottom += images[i].contentData + "\n\n";
+            }
+
+            //TODO: load css from included stylesheet
+            var styles = "";
+
+            // Aggregate parts of the file together 
+            var fileContent = static.mhtml.top.replace("_html_", static.mhtml.head.replace("_styles_", styles) + static.mhtml.body.replace("_body_", markup.html())) + mhtmlBottom;
+            mhtmlBottom += "--NEXT.ITEM-BOUNDARY--";
+
+            // Create a Blob with the file contents
+            var blob = new Blob([content], {
+                type: "application/msword;charset=utf-8"
+            });
+            saveAs(blob, fileName + ".doc");
+        };
+    })(jQuery);
+} else {
+    if (!jQuery) {
+        console.error("jQuery Word Export: missing dependency (jQuery)");
+    }
+    if (!saveAs) {
+        console.error("jQuery Word Export: missing dependency (FileSaver.js)");
+    };
+}
